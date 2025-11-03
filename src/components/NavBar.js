8a801e85ec5224af779afa40f5e3d1b25e4308b7
rebahama <97362222+rebahama@users.noreply.github.com>
2025-11-03 00:42:14 +0000

    style navbar and showall page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,35 +8,70 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <Navbar expand="lg" className={styles.NavBar}>
+    <Navbar expand="lg" className={`${styles.NavBar} shadow-sm`} fixed="top">
       <Container>
-        <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+        {/* Brand */}
+        <Navbar.Brand as={Link} to="/" className={styles.brand}>
+          <wa-icon name="tools" class="me-2"></wa-icon>
+          Mechanic AI
+        </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
-              <wa-icon slot="start" name="house"></wa-icon> Home
+          <Nav className={`me-auto align-items-center ${styles.navLinks}`}>
+            <Nav.Link as={Link} to="/" className={styles.navItem}>
+              <wa-icon name="house" class="me-1"></wa-icon>
+              Home
             </Nav.Link>
 
-            <Nav.Link as={Link} to="/link">Link</Nav.Link>
+            <Nav.Link as={Link} to="/about" className={styles.navItem}>
+              <wa-icon name="info-circle" class="me-1"></wa-icon>
+              About
+            </Nav.Link>
 
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/signup">Sign up</Nav.Link>
-             <Nav.Link as={Link} to="/showall">Show all</Nav.Link>
+            <Nav.Link as={Link} to="/signup" className={styles.navItem}>
+              <wa-icon name="user-plus" class="me-1"></wa-icon>
+              Sign Up
+            </Nav.Link>
 
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item>Action</NavDropdown.Item>
-              <NavDropdown.Item>Another action</NavDropdown.Item>
-              <NavDropdown.Item>Something</NavDropdown.Item>
+            <Nav.Link as={Link} to="/showall" className={styles.navItem}>
+              <wa-icon name="list" class="me-1"></wa-icon>
+              Show All
+            </Nav.Link>
+
+            <NavDropdown
+              title={
+                <>
+                  <wa-icon name="chevron-down" class="me-1"></wa-icon>
+                  More
+                </>
+              }
+              id="basic-nav-dropdown"
+              className={styles.dropdown}
+            >
+              <NavDropdown.Item className={styles.dropdownItem}>
+                <wa-icon name="wrench" class="me-2"></wa-icon>
+                Action
+              </NavDropdown.Item>
+              <NavDropdown.Item className={styles.dropdownItem}>
+                <wa-icon name="gear" class="me-2"></wa-icon>
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item className={styles.dropdownItem}>
+                <wa-icon name="clipboard-list" class="me-2"></wa-icon>
+                Something
+              </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item>Separated link</NavDropdown.Item>
+              <NavDropdown.Item className={styles.dropdownItem}>
+                <wa-icon name="dash" class="me-2"></wa-icon>
+                Separated link
+              </NavDropdown.Item>
             </NavDropdown>
-
-            <div className="ms-3">
-              <wa-avatar label="User avatar"></wa-avatar>
-            </div>
           </Nav>
+
+          <div className="ms-auto d-flex align-items-center">
+            <wa-avatar label="User avatar"></wa-avatar>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
