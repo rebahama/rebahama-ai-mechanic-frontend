8cdc64b10fefa64824f3ea3c520c3a8ff723844e
rebahama <97362222+rebahama@users.noreply.github.com>
2025-11-03 23:25:54 +0000

    add currentuser context and display navbar based on logged in or not

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,13 +4,17 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import styles from '../styles/NavBar.module.css';
+import {
+  useCurrentUser,
+} from "../contexts/CurrentUserContext";
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+  const currentUser = useCurrentUser(); // Get current user from context
+
   return (
     <Navbar expand="lg" className={`${styles.NavBar} shadow-sm`} fixed="top">
       <Container>
-        {/* Brand */}
         <Navbar.Brand as={Link} to="/" className={styles.brand}>
           <wa-icon name="tools" class="me-2"></wa-icon>
           Mechanic AI
@@ -29,48 +33,28 @@ const NavBar = () => {
               About
             </Nav.Link>
 
-            <Nav.Link as={Link} to="/signup" className={styles.navItem}>
-              <wa-icon name="user-plus" class="me-1"></wa-icon>
-              Sign Up
-            </Nav.Link>
+            { !currentUser && (
+              <Nav.Link as={Link} to="/signup" className={styles.navItem}>
+                <wa-icon name="user-plus" class="me-1"></wa-icon>
+                Sign Up
+              </Nav.Link>
+            )}
 
             <Nav.Link as={Link} to="/showall" className={styles.navItem}>
               <wa-icon name="list" class="me-1"></wa-icon>
               Show All
             </Nav.Link>
-
-            <NavDropdown
-              title={
-                <>
-                  <wa-icon name="chevron-down" class="me-1"></wa-icon>
-                  More
-                </>
-              }
-              id="basic-nav-dropdown"
-              className={styles.dropdown}
-            >
-              <NavDropdown.Item className={styles.dropdownItem}>
-                <wa-icon name="wrench" class="me-2"></wa-icon>
-                Action
-              </NavDropdown.Item>
-              <NavDropdown.Item className={styles.dropdownItem}>
-                <wa-icon name="gear" class="me-2"></wa-icon>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item className={styles.dropdownItem}>
-                <wa-icon name="clipboard-list" class="me-2"></wa-icon>
-                Something
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item className={styles.dropdownItem}>
-                <wa-icon name="dash" class="me-2"></wa-icon>
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
 
           <div className="ms-auto d-flex align-items-center">
-            <wa-avatar label="User avatar"></wa-avatar>
+            {currentUser ? (
+              <wa-avatar label={currentUser.username}></wa-avatar>
+            ) : (
+              <Nav.Link as={Link} to="/login" className={styles.navItem}>
+                <wa-icon name="sign-in" class="me-1"></wa-icon>
+                Sign In
+              </Nav.Link>
+            )}
           </div>
         </Navbar.Collapse>
       </Container>
